428a9c9c37f2e500d22f97f7ed6d23e0ffe3cb2c
Jean Perrault <perrault.jean66@gmail.com>
2019-02-19 16:01:58 +0100

    -

---
+++ public/superglobal.php
@@ -0,0 +1,24 @@
+<?php
+
+// variable
+$variable = 42;
+
+// constante
+define('CONSTANTE', "hello");
+
+// Super Global
+
+// GET
+var_dump( $_GET); // renvoi un tableau
+
+// POST
+var_dump( $_POST); // renvoi un tableau nourri par PHP
+
+// SESSION
+var_dump( $_SESSION); 
+
+// SERVER
+var_dump( $_SERVER); // renvoi phpinfo()
+
+
+
