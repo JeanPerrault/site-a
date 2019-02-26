6136a94215de54812dabbe8dee79626cd3cfc686
Jean Perrault <perrault.jean66@gmail.com>
2019-02-26 16:58:51 +0100

    -

--- public/security/config.php
+++ public/security/config.php
@@ -9,7 +9,7 @@ session_start();
 
 // Connexion à la BDD
 try {
-    $db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=webpizza;charset=utf8', 'root', 'myosw3sql');
+    $db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=webpizza;charset=utf8', 'root', '');
 } catch( PDOException $e ) {
     echo $e->getMessage();
     die();
